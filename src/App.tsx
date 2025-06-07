e1e76ded2b873e910af0d325527dd8923587c8ad
Elgendy Store <myuofo2005@gmail.com>
2025-06-07 11:38:49 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BudgetProvider } from './context/BudgetContext';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import HomePage from './pages/HomePage';
+import CategoryPage from './pages/CategoryPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import BudgetPage from './pages/BudgetPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import FAQPage from './pages/FAQPage';
+import SearchResultsPage from './pages/SearchResultsPage';
+
+function App() {
+  return (
+    <Router>
+      <BudgetProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/categories/:id" element={<CategoryPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+            <Route path="/budget" element={<BudgetPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/faq" element={<FAQPage />} />
+            <Route path="/search" element={<SearchResultsPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BudgetProvider>
+    </Router>
+  );
+}
+
+export default App;
